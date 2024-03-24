87585b2ec44a90c21421f1bacd56238123e9e653
Ala <ala@Alas-MacBook-Pro.local>
2024-03-24 19:20:10 +0200

    ex2

--- e02/src/main/java/myapp/Account.java
+++ e02/src/main/java/myapp/Account.java
@@ -1,26 +1,40 @@
 package myapp;
 
+import javax.imageio.IIOException;
+
 public class Account {
     private String accountNumber;
     private double balance;
 
     public Account(String accountNumber) {
-
+        this.accountNumber = accountNumber;
+        this.balance = 0;
     }
 
     public void deposit(double amount) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if (amount < 0) {
+            throw new IllegalArgumentException("The value cannot be negative.");
+        }
+        balance+=amount;
     }
 
     public void withdraw(double amount) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if (amount < 0) {
+            throw new IllegalArgumentException("The value cannot be negative.");
+        }
+        if (balance-amount >= 0) {
+            balance -= amount;
+        }
+        else {
+            throw new IllegalArgumentException("Insufficient amount of money.");
+        }
     }
 
     public double getBalance() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return balance;
     }
 
     public String getAccountNumber() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return accountNumber;
     }
 }
--- e02/src/main/java/myapp/SimpleBank.java
+++ e02/src/main/java/myapp/SimpleBank.java
@@ -7,17 +7,29 @@ public class SimpleBank {
     private Map<String, Account> accounts;
 
     public SimpleBank() {
+        this.accounts = new HashMap<>();
     }
 
     public void addAccount(Account account) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if (accounts.containsKey(account.getAccountNumber())) {
+            throw new IllegalArgumentException("The account already exists.");
+        }
+        accounts.put(account.getAccountNumber(), account);
     }
 
     public Account getAccount(String accountNumber) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return accounts.get(accountNumber);
     }
 
     public void transfer(String fromAccountNumber, String toAccountNumber, double amount) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if (!accounts.containsKey(toAccountNumber) || !accounts.containsKey(fromAccountNumber)) {
+            throw new IllegalArgumentException("Account not found.");
+        }     
+        else if (accounts.get(fromAccountNumber).getBalance() - amount < 0 ) {
+            throw new IllegalArgumentException("Not enough funds.");
+        }
+        accounts.get(fromAccountNumber).withdraw(amount); //take money from account
+        accounts.get(toAccountNumber).deposit(amount); //put money in account
+
     }
 }
